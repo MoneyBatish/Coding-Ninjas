26ea363251f5ee3db938d98ec95e9873db2776ac
moneybatish <money0465.be20@chitkara.edu.in>
2022-08-17 17:37:20 +0530

    remove x

---
+++ recursion1b/remove_x.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void removeX(char input[]) {
+    if(input[0]=='\0'){
+        return;
+    }
+    removeX(input+1);
+    if(input[0]=='x'){
+        int len = strlen(input);
+        for(int i=0;i<=len;i++){
+            input[i]=input[i+1];
+        }
+    }
+
+}
+
+int main() {
+    char input[100];
+    cin.getline(input, 100);
+    removeX(input);
+    cout << input << endl;
+}
